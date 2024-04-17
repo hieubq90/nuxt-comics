07b76409313e3a46a041a75798063f2cb8eff053
hieubq <hieubq>
2024-04-17 23:55:32 +0700

    adding module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/device",
+    "@nuxt/fonts",
+    "@nuxtjs/color-mode",
+    "@vue-macros/nuxt",
+    "@unocss/nuxt",
+    "nuxt-swiper",
+    "nuxt-icon",
+  ],
+  macros: {
+    // configure plugin options, if needed
+  },
+});
